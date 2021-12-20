9a1b2e2345c4a03319e42c18f8d6f46a6a7b2048
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-12-20 17:34:19 -0500

    CRUD functions added, middleware working, 13/13 tests passing

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -19,8 +19,20 @@ const checkCarId = async (req, res, next) => {
 
 const checkCarPayload = (req, res, next) => {
   const {vin, make, model, mileage} = req.body
-  if(!vin || !make || !model || !mileage){
-    res.status(400).json({message:`${req.body} is missing`}) //this might not work - req.body was destrctured
+  if(!vin){
+    res.status(400).json({message:`vin is missing`})
+  }
+  else if (!make){
+    res.status(400).json({message:`make is missing`})
+  }
+  else if (!model){
+    res.status(400).json({message:`model is missing`})
+  }
+  else if (!mileage){
+    res.status(400).json({message:`mileage is missing`})
+  }
+  else {
+    next()
   }
 }
 
@@ -43,7 +55,11 @@ const checkVinNumberUnique = async (req, res, next) => {
 
     if(existingVin){
       res.status(400).json({message:`vin ${req.body.vin} already exists`})
+    } else {
+      next()
     }
+  } catch(err){
+    next(err) // this might break server
   }
 }
 
